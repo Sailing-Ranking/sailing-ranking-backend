122c32d47d8a26e0f1fec48aa77e42a1cfd660f2
michael-gvdw <michael@fragrant-chios.com>
2022-10-19 08:45:00 +0200

    Implemented Competitor Route Endpoints

--- api/__init__.py
+++ api/__init__.py
@@ -24,6 +24,10 @@ def create_app():
 
     app.include_router(competition.router)
 
+    from api.routes import competitor
+
+    app.include_router(competitor.router)
+
     @app.get("/health-check", status_code=status.HTTP_200_OK)
     def health_check() -> dict[str, bool]:
         return {"healthy": True}
