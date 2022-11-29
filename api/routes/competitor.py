41f91e7fa8d13a7b3e0bccdc9b226b42330c5b95
michael-gvdw <michael@fragrant-chios.com>
2022-11-29 10:02:19 +0100

    Fix logic in competitor creation

--- api/routes/competitor.py
+++ api/routes/competitor.py
@@ -5,7 +5,7 @@ from pydantic import UUID4
 from sqlalchemy.orm import Session
 
 from api import get_db
-from api.models import Competitor
+from api.models import Competitor, competitor
 from api.schemas.competitor import CompetitorCreate, CompetitorOut, CompetitorUpdated
 from api.schemas.position import PositionOut
 
@@ -51,9 +51,14 @@ async def get_competitor_positions(id: UUID4, db: Session = Depends(get_db)):
 async def create(create_competitor: CompetitorCreate, db: Session = Depends(get_db)):
     """Handle creating a competitor."""
 
+    if not db.query(competitor).get(create_competitor.competition_id):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="competition not found"
+        )
+
     if (
         db.query(Competitor)
-        .filter(Competitor.sail_nr == create_competitor.sail_nr)
+        .filter(Competitor.competition_id == create_competitor.competition_id , Competitor.sail_nr == create_competitor.sail_nr)
         .first()
     ):
         raise HTTPException(
